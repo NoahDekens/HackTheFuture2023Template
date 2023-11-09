8becd982e1f915c168083a2d6bff071682c1be31
TimHoremans <tim.horemans97@gmail.com>
2023-11-09 14:43:38 +0100

    bug bad request

--- SolutionPathB/Solution/Challenge3-B.cs
+++ SolutionPathB/Solution/Challenge3-B.cs
@@ -1,10 +1,6 @@
 ﻿using Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http.Json;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace SolutionPathB.Solution
 {
@@ -14,9 +10,9 @@ namespace SolutionPathB.Solution
 
 		private const string sampleEndpoint = "/api/path/b/hard/sample";
 
-		Dictionary<char,int> lookup = new Dictionary<char, int>()
+		Dictionary<char, int> lookup = new Dictionary<char, int>()
 			{
-				{ '.', 1 },
+				{ '·', 1 },
 				{ '|', 5 }
 			};
 
@@ -33,18 +29,37 @@ namespace SolutionPathB.Solution
 
 			await SolveChallenge(hackTheFutureClient);
 		}
-		
 
-		public async Task SolveChallenge(HackTheFutureClient hackTheFutureClient)
+		public async Task PostAnswer(HackTheFutureClient hackTheFutureClient, string result)
 		{
-			var response = await hackTheFutureClient.GetAsync(sampleEndpoint);
+			var response = await hackTheFutureClient.PostAsJsonAsync(sampleEndpoint, result);
 
 			var content = await response.Content.ReadAsStringAsync();
 
-			string test = "| |";
-			var terms = test.Split(" ");
+			Console.WriteLine(response.StatusCode);
+
+		}
+
+		public async Task SolveChallenge(HackTheFutureClient hackTheFutureClient)
+		{
+			List<string> response = await hackTheFutureClient.GetFromJsonAsync<List<string>>(sampleEndpoint);
+
+			int deciamResult = 0;
+			foreach (var item in response)
+			{
+				deciamResult += Solve(item);
+			}
+
+			string reverseResult = SolveReverse(deciamResult);
+
+			await PostAnswer(hackTheFutureClient, reverseResult);
+
+		}
+		public int Solve(string ex)
+		{
+			var terms = ex.Split(" ");
 			int cumulative = 0;
-			
+
 			for (int i = 0; i < terms.Length; i++)
 			{
 				var term = terms[terms.Length - i - 1];
@@ -55,7 +70,40 @@ namespace SolutionPathB.Solution
 				}
 				cumulative += innerCumulative * (int)Math.Pow(20, i);
 			}
-			
+			return cumulative;
+		}
+
+
+		string SolveReverse(int value)
+		{
+			var remainder = value;
+
+			var result = new List<string>();
+			do
+			{
+				var temp = remainder / 20;
+				var term = remainder % 20;
+
+				var count5s = term / 5;
+				int rem = term % 5;
+
+				result.Add($"{new string('|', count5s)}{new string('.', rem)}");
+
+				remainder = temp;
+
+			} while (remainder > 0);
+
+			var builder = new StringBuilder();
+
+			result.Reverse();
+
+			foreach (var term in result)
+			{
+				builder.Append(term);
+				builder.Append(' ');
+			}
+
+			return builder.ToString();
 		}
 
 	}
